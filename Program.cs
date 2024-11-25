2b58d971fcb2768db861ffead95d212c5641d85f
AngelF <angel-abraham.flores.7e8@itb.cat>
2024-11-25 12:41:19 +0100

    Solucion Ejercicio 2.1. Closes #3

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.CompilerServices;
 namespace acfour_angel_Flores
 {
     public class Acfour
@@ -7,7 +8,8 @@ namespace acfour_angel_Flores
         {
             const string Prime = "Introdueix quants números vols introduir";
             const string Num = "Introdueix un número de la llista";
-            const string Mostra = "Els números introduÏts són ";
+            const string Mostra = "Els números introduÏts són: ";
+            const string Ordenat = "Els números ordenats de més gran a més petit són: ";
             const string Error = "Error de programa";
             int num;
             try
@@ -22,6 +24,24 @@ namespace acfour_angel_Flores
 
                 }
                 Console.WriteLine(Mostra);
+                for (int i = 0; i < num; i++)
+                {
+                    Console.Write("{0} ", quantitat[i]);
+                }
+                for (int i = 0; i < num - 1; i++)
+                {
+                    for (int j = i + 1; j < num; j++)
+                    {
+                        if (quantitat[i] < quantitat[j])
+                        {
+                            int aux = quantitat[i];
+                            quantitat[i] = quantitat[j];
+                            quantitat[j] = aux;
+                        }
+                    }
+                }
+                Console.WriteLine("\n");
+                Console.WriteLine(Ordenat);
                 for (int i = 0; i < num; i++)
                 {
                     Console.Write("{0} ", quantitat[i]);
