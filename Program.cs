d5cedf63ca534c1c1d7614f1ffaa4d4b47a668d9
AngelF <angel-abraham.flores.7e8@itb.cat>
2024-11-25 12:28:08 +0100

    Solucion Ejercicio2. Closes #1

--- Program.cs
+++ Program.cs
@@ -1,1 +1,36 @@
-﻿
+﻿using System;
+namespace acfour_angel_Flores
+{
+    public class Acfour
+    {
+        public static void Main()
+        {
+            const string Prime = "Introdueix quants números vols introduir";
+            const string Num = "Introdueix un número de la llista";
+            const string Mostra = "Els números introduÏts són ";
+            const string Error = "Error de programa";
+            int num;
+            try
+            {
+                Console.WriteLine(Prime);
+                num = Convert.ToInt32(Console.ReadLine());
+                int[] quantitat = new int[num];
+                for (int i = 0; i < num; i++)
+                {
+                    Console.WriteLine(Num);
+                    quantitat[i] = Convert.ToInt32(Console.ReadLine());
+
+                }
+                Console.WriteLine(Mostra);
+                for (int i = 0; i < num; i++)
+                {
+                    Console.Write("{0} ", quantitat[i]);
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine(Error);
+            }
+        }
+    }
+}
